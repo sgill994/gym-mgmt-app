de88b332daa12792d772ed80971174eee7bccef1
asidhu9 <asidhu994@gmail.com>
2024-05-28 16:07:36 -0700

    new form start

---
+++ src/components/new-customer-form/new-customer-form.tsx
@@ -0,0 +1,17 @@
+import './new-customer-form.css';
+import React from 'react';
+
+export interface NewCustomerFormProps {
+    className?: string;
+}
+
+export const NewCustomerForm: React.FC<NewCustomerFormProps> = ({ className = '' }) => (
+    <div className={className}>
+<form>
+<div name='hi' className="customerInfo">
+<form><label>First name:</label><br /><input type="text" /><br /><label>Last name:</label><br /><input type="text" /><br /><br /></form>
+</div>
+<div className="additionalnfo">
+<form><label>First name:</label><br /><input type="text" /><br /><label>Last name:</label><br /><input type="text" /><br /><br /></form>
+</div></form></div>
+);
