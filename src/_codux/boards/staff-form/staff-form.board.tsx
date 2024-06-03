fe31bde3339597816176a7e059f903372a25d4c0
sgill994 <savvy_gill@hotmail.com>
2024-06-03 15:48:38 -0700

    deleted staff info component (codux)

--- src/_codux/boards/staff-form/staff-form.board.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createBoard } from '@wixc3/react-board';
-import { StaffForm } from '../../../components/staff-form/staff-form';
-
-export default createBoard({
-    name: 'StaffForm',
-    Board: () => <StaffForm />,
-    isSnippet: true,
-});
