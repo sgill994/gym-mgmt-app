b6579879543f15cff6eea882745108f8c75da75a
sgill994 <savvy_gill@hotmail.com>
2024-06-04 15:41:49 -0700

    moved table for active classes from manageclassespage to classlist

--- src/components/ClassList.js
+++ src/components/ClassList.js
@@ -1,14 +1,51 @@
 import React from 'react';
+import { Table } from 'react-bootstrap'
+import ClassDetails from '../components/ClassDetails';
 
-const ClassList = ({ classes }) => (
-  <ul id="classes-list" className="class-list-group">
-    {classes.map((course, index) => (
-      <li key={index} className="class-list-item">
-        {course.title} on {course.days} at {course.time} for {course.length} - Instructor: {course.instructor} 
-        {course.reservationLimit && ` - Class Limit: ${course.reservationLimit}`}
-      </li>
-    ))}
-  </ul>
-);
+
+const ClassList = ({ classes }) => {
+  const openClassDetails = (course) => {
+    const newWindow = window.open('', '', 'width=600,height=400');
+    newWindow.document.write('<html><head><title>Class Details</title></head><body>')
+    newWindow.document.write('<div id="root"></div>');
+    newWindow.document.write('</body></html>');
+    newWindow.document.close();
+
+    newWindow.addEventListener('DOMContentLoaded', () => {
+        const root = createRoot(newWindow.document.getElementById('course-name'))
+        root.render(<ClassDetails course={course} />);
+    });
+  };
+  
+  return (
+    <Table striped bordered hover>
+      <thead>
+      <tr>
+          <th>Name</th>
+          <th>Weekly Classes</th>
+          <th>Start Time</th>
+          <th>Length</th>
+          <th>Class Limit</th>
+          <th>Instructor</th>
+      </tr>
+      </thead>
+      <tbody>
+      {classes.map((course, index) => (
+          <tr key={index}>
+          <td>
+              <a href="#" style={{ color: 'blue', textDecoration: 'underline' }} onClick={() => openClassDetails(course)}>
+                  {course.title}</a>
+          </td>
+          <td>{course.days.split(', ').length}</td> 
+          <td>{course.time}</td>
+          <td>{course.length}</td>
+          <td>{course.reservationLimit ? course.reservationLimit : 'No limit'}</td>
+          <td>{course.instructor}</td>
+          </tr>
+      ))}
+      </tbody>
+    </Table>
+  );
+}
 
 export default ClassList;
--- src/pages/ManageClassesPage.js
+++ src/pages/ManageClassesPage.js
@@ -1,27 +1,14 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Modal, Button, Table } from 'react-bootstrap'
-import ClassDetails from '../components/ClassDetails';
 import NewClassForm from '../components/NewClassForm';
+import ClassList from '../components/ClassList';
 
 const ManageClassesPage = ({ classes, addClass }) => {
     const [showModal, setShowModal] = useState(false);
 
     const handleOpen = () => setShowModal(true);
     const handleClose = () => setShowModal(false);
-
-    const openClassDetails = (course) => {
-        const newWindow = window.open('', '', 'width=600,height=400');
-        newWindow.document.write('<html><head><title>Class Details</title></head><body>')
-        newWindow.document.write('<div id="root"></div>');
-        newWindow.document.write('</body></html>');
-        newWindow.document.close();
-
-        newWindow.addEventListener('DOMContentLoaded', () => {
-            const root = createRoot(newWindow.document.getElementById('course-name'))
-            root.render(<ClassDetails course={course} />);
-        });
-    };
     
     return (
         <div id="manage-classes" className="tab active">
@@ -36,33 +23,7 @@ const ManageClassesPage = ({ classes, addClass }) => {
                 </Modal.Body>
             </Modal>
             <h2>Active Classes</h2>
-            <Table striped bordered hover>
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Weekly Classes</th>
-                    <th>Start Time</th>
-                    <th>Length</th>
-                    <th>Class Limit</th>
-                    <th>Instructor</th>
-                </tr>
-                </thead>
-                <tbody>
-                {classes.map((course, index) => (
-                    <tr key={index}>
-                    <td>
-                        <a href="#" style={{ color: 'blue', textDecoration: 'underline' }} onClick={() => openClassDetails(course)}>
-                            {course.title}</a>
-                    </td>
-                    <td>{course.days.split(', ').length}</td> 
-                    <td>{course.time}</td>
-                    <td>{course.length}</td>
-                    <td>{course.reservationLimit ? course.reservationLimit : 'No limit'}</td>
-                    <td>{course.instructor}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </Table>
+            <ClassList classes={classes} />
         </div>
     );
 }
