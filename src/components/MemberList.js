82af6d0d51c78b7ba9c8f44a752a3d70ac3b57af
asidhu9 <asidhu994@gmail.com>
2024-06-03 20:41:28 -0700

    More form stuff

--- src/components/MemberList.js
+++ src/components/MemberList.js
@@ -4,7 +4,7 @@ const MemberList = ({ members }) => (
   <ul id="member-list">
     {members.map((member, index) => (
       <li key={index}>
-        {member.firstName} {member.lastName} - {member.contactNumber}
+        {member.firstName} {member.lastName} - {member.phoneNumber}
       </li>
     ))}
   </ul>
