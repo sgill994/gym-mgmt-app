ce83615f7f178fb103e1948d17c5134c083e3620
asidhu9 <asidhu994@gmail.com>
2024-06-04 17:31:01 -0700

    form 2
    half way done the form

--- src/components/NewMemberForm.js
+++ src/components/NewMemberForm.js
@@ -1,7 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap-datepicker/dist/js/bootstrap-datepicker';
+import $ from 'jquery'; // Import jQuery
 
 const NewMemberForm = ({ addMember }) => {
+
+  useEffect(() => {
+    $('#datepicker').datepicker({
+      format: 'mm/dd/yyyy', // specify your desired format
+      autoclose: true
+      // Other options you might need...
+    });
+  }, []);
 
   const [validated, setValidated] = useState(false);
 
@@ -28,57 +37,64 @@ const NewMemberForm = ({ addMember }) => {
       setValidated(true);
     }
 
-    useEffect(() => {
-      $('#datepicker').datepicker({
-        format: 'mm/dd/yyyy', // specify your desired format
-        autoclose: true
-        // Other options you might need...
-      });
-    }, []);
-
   };
   
   return (
     <form className={`row g-3 ${validated ? 'was-validated' : ''}`} onSubmit={handleSubmit} noValidate >
           
       <div className = 'customerWelcome'>
-      <legend>We are welcoming..</legend>
-          <label htmlFor="validationCustom01" className="form-label">First name</label>
-          <input type="text" className="form-control" id="validationCustom01" placeholder="First Name" name="firstName" required />
-          <div className="invalid-feedback">Please add First Name!</div>
-        <br/>
-          <label htmlFor="validationCustom02" className="form-label">Last name</label>
-          <input type="text" className="form-control" id="validationCustom02" placeholder="Last Name" name="lastName" required />
-          <div className="invalid-feedback">Looks good!</div>
-        <br />
-          <label htmlFor="validationCustom03" className="form-label">Phone Number</label>
-          <input type="text" className="form-control" id="validationCustom03" placeholder="Phone Number" name="phoneNumber" required />
-          <div className="invalid-feedback">Looks good!</div>
-        <br />
-          <label htmlFor="validationCustom03" className="form-label">Email</label>
-          <input type="text" className="form-control" id="validationCustom04" placeholder="Email" name="email" required />
-          <div className="invalid-feedback">Looks good!</div>
-       <br/>
+        <legend>We are welcoming..</legend>
+        <div class="mb-3">
+            <label htmlFor="validationCustom01" className="form-label">First name</label>
+            <input type="text" className="form-control" id="validationCustom01" placeholder="First Name" name="firstName" required />
+            <div className="invalid-feedback">Please add First Name!</div>
+        </div>
+        <div class="mb-1">
+            <label htmlFor="validationCustom02" className="form-label">Last name</label>
+            <input type="text" className="form-control" id="validationCustom02" placeholder="Last Name" name="lastName" required />
+            <div className="invalid-feedback">Looks good!</div>
+        </div>
+        <div class="mb-1">
+            <label htmlFor="validationCustom03" className="form-label">Phone Number</label>
+            <input type="text" className="form-control" id="validationCustom03" placeholder="Phone Number" name="phoneNumber" required />
+            <div className="invalid-feedback">Looks good!</div>
+        </div>
+        <div class="mb-1">
+            <label htmlFor="validationCustom04" className="form-label">Email</label>
+            <input type="text" className="form-control" id="validationCustom04" placeholder="Email" name="email" required />
+            <div className="invalid-feedback">Looks good!</div>
+        </div>
+        <div class="mb-1">
           <input  type="checkbox" value="notSub" id="flexCheckDefault"/>
-          <label for="flexCheckDefault">
+          <label htmlFor="flexCheckDefault">
             Mark as unsubscibed
           </label>
+        </div>
       </div>
 
       <div className = 'customerAdditionalInfo'>
-      <legend>Additonal Info</legend>
-
-        <label htmlFor="validationCustom03" className="form-label">Customer Notes</label>
-        <br/>
-        <textarea className="form-control" placeholder="Leave a comment here" style={{width:'100%'}} name="customerComments"></textarea>
-        <br/>
-        <div class="input-group date" data-provide="datepicker">
-          <input type="text" class="form-control"/>
-          <div class="input-group-addon">
-              <span class="glyphicon glyphicon-th"></span>
+        <legend>Additonal Info</legend>
+        <div class="mb-3">
+          <label htmlFor="validationCustom05" className="form-label">Customer Notes</label>
+          <textarea className="form-control" placeholder="Leave a comment here" id="validationCustom05" name="customerComments"></textarea>
+        </div>
+        <div class="mb-1">
+          <label htmlFor="validationCustom06" className="form-label">Date of Birth</label><br/>
+          <div className="input-group date" data-provide="datepicker">
+            <input type="text" className="form-control" id='validationCustom06' name="dateOfBirth"/>
+            <div className="input-group-addon">
+              <span className="glyphicon glyphicon-th"></span>
+            </div>
           </div>
-      </div>
-
+        </div>
+        <div class="mb-1">
+            <label htmlFor="validationCustom07" className="form-label">Referral Source</label>
+            <input type="text" className="form-control" id="validationCustom07" placeholder="Email" name="referralSource" />
+        </div>
+        <div class="mb-1">
+            <label htmlFor="validationCustom08" className="form-label">Custom Customer ID</label>
+            <input type="text" className="form-control" id="validationCustom08" placeholder="Email" name="customCustomerID" />
+        </div>
       </div>
 
       <div className = 'customerAddress'>
